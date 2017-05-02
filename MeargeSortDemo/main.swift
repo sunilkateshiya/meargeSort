70bec9543e0af982f9e440686b18fb9c4d45941c
jetanikalpesh <jetanikalpesh@gmail.com>
2017-05-02 10:02:08 +0530

    hh

--- MeargeSortDemo/main.swift
+++ MeargeSortDemo/main.swift
@@ -8,5 +8,33 @@
 
 import Foundation
 
-print("Hello, World!")
+func merge(left:[Int],right:[Int]) -> [Int] {
+    var mergedList = [Int]()
+    var left = left
+    var right = right
+    
+    while left.count > 0 && right.count > 0 {
+        if left.first! < right.first! {
+            mergedList.append(left.removeFirst())
+        } else {
+            mergedList.append(right.removeFirst())
+        }
+    }
+    
+    return mergedList + left + right
+}
 
+func mergeSort(list:[Int]) -> [Int] {
+    guard list.count > 1 else {
+        return list
+    }
+    
+    let leftList = Array(list[0..<list.count/2])
+    let rightList = Array(list[list.count/2..<list.count])
+    
+    return merge(left: mergeSort(list:leftList), right: mergeSort(list:rightList))
+}
+var list = [1,32,45,7,97,14,65,42,78,36,97,1,0]
+
+
+print(mergeSort(list: list))
